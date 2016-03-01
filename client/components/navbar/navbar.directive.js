de69107eb3d8538d868a0ec8f9df117b1d84d2c3
aniknafs <niknafs@gmail.com>
2016-02-29 18:33:22 -0800

    initial commit

---
+++ client/components/navbar/navbar.directive.js
@@ -0,0 +1,9 @@
+'use strict';
+
+angular.module('onikioApp')
+  .directive('navbar', () => ({
+    templateUrl: 'components/navbar/navbar.html',
+    restrict: 'E',
+    controller: 'NavbarController',
+    controllerAs: 'nav'
+  }));
